eea9c875fa0842a40d4461f8674d46d2a766f978
Yanina Charas <ycharas@gmail.com>
2022-09-06 16:46:51 -0300

    agregue el manejo de login y el manejo de errores en index,html

--- userblog/views.py
+++ userblog/views.py
@@ -1,29 +1,32 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 def login_request(request):
-    # if request.method == "POST":
-    #     form = AuthenticationForm(request, data=request.POST)
-    #
-    #     if [form.is](http://form.is)_valid():
-    #         data = form.cleaned_data
-    #
-    #         usuario = data.get('username')
-    #         contrasenia = data.get('password')
-    #
-    #         user = authenticate(username=usuario, password=contrasenia)
-    #
-    #         if user:
-    #             login(request, user)
-    #             [messages.info](http://messages.info)(request, 'Inicio de sesion satisfactorio!')
-    #
-    #         else:
-    #             [messages.info](http://messages.info)(request, 'inicio de sesion fallido!')
-    #     else:
-    #         [messages.info](http://messages.info)(request, 'inicio de sesion fallido!')
-    #
-    #     return redirect('AppCoderInicio')
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+
+            usuario = data.get('username')
+            contrasenia = data.get('password')
+
+            user = authenticate(username=usuario, password=contrasenia)
+
+            if user:
+                login(request, user)
+                messages.info(request, 'Inicio de sesion satisfactorio!')
+
+            else:
+                messages.info(request, 'inicio de sesion fallido!')
+        else:
+            messages.info(request, 'inicio de sesion fallido!')
+
+        return redirect('AppBlogInicio')
 
     contexto = {
         'form': AuthenticationForm(),
